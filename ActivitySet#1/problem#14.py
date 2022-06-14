549da7fd2498824df27232f0824dd7533c929cfc
Shetty10 <97089508+Shettyv10@users.noreply.github.com>
2022-06-14 09:26:21 +0000

    ..

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,2 +1,4 @@
 # Using Web Services
 # https://www.py4e.com/lessons/servces
+x=int(input("enter the age of the individual"))
+print("you are eligible to vote") else print("no you arenot eligible to vote")
