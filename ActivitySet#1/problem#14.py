7f79b371efeb0acb2832e498f67a3f4c5f165c03
Shetty10 <97089508+Shettyv10@users.noreply.github.com>
2022-07-07 18:23:24 +0000

    ..

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -1,4 +1,22 @@
-# Using Web Services
-# https://www.py4e.com/lessons/servces
-x=int(input("enter the age of the individual"))
-print("you are eligible to vote") else print("no you arenot eligible to vote")
+import urllib
+import xml.etree.ElementTree as ET
+
+url = raw_input("Enter location: ")
+if len(url) < 1:
+    url = "http://python-data.dr-chuck.net/comments_200531.xml"
+print "Retrieving " + url
+
+xml = urllib.urlopen(url).read()
+print "Retrieved: " + str(len(xml)) + " characters"
+
+tree = ET.fromstring(xml)
+
+counts =  tree.findall('.//count')
+print "Count: " + str(len(counts))
+
+accumulator = 0
+
+for count in counts:
+    accumulator += int(count.text)
+
+print "Sum:" + str(accumulator)
